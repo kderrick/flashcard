2648c06a3ad82e620290da4f896fd986a6216b4a
kd and rk <rk>
2016-01-11 13:50:20 -0800

    add toggle function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,8 +1,34 @@
 $(function(){
-  $("h2").click(function(){
-    $("h2").fadeToggle();
+  $("#modulo").click(function(){
+    $(".hiddenModulo").toggle();
+    $(".defShownModulo").toggle();
   });
-  $("p").dblclick(function(){
-    alert("This is a paragraph.");
+});
+
+$(function(){
+  $("#string").click(function(){
+    $(".hiddenString").toggle();
+    $(".defShownString").toggle();
+  });
+});
+
+$(function(){
+  $("#parameter").click(function(){
+    $(".hiddenParameter").toggle();
+    $(".defShownParameter").toggle();
+  });
+});
+
+$(function(){
+  $("#booleans").click(function(){
+    $(".hiddenBooleans").toggle();
+    $(".defShownBooleans").toggle();
+  });
+});
+
+$(function(){
+  $("#alert").click(function(){
+    $(".hiddenAlert").toggle();
+    $(".defShownAlert").toggle();
   });
 });
